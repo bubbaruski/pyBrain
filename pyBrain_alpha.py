1423d96956e8e1faaf4d765062944f2194e9a4a0
Matthew Ognibene <mattognibene1@gmail.com>
2017-05-15 21:20:12 -0400

    Hyper parameters: not much really

--- pyBrain_alpha.py
+++ pyBrain_alpha.py
@@ -1,3 +1,13 @@
 #Welcome to pyBrain Alpha 1.0 developed my Matthew Ognibene 14 May 2017
+#My third NN project, now with multivariable calc and linear algebra!
+import numpy as np
 
-print ("Hello Github!")
+class Network():
+    def __init__(self,numOfInputs,numOfOutputs,hiddenSize):
+        #Hyperparameters
+        #NOTE: In this version of pyBrain, there is only ONE hidden layer
+        self.numOfInputs=numOfInputs
+        self.numOfOutputs = numOfOutputs
+        self.hiddenSize = hiddenSize
+
+
